777f9c8626b7f546b92fc0ac881808344d0f500c
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2019-12-16 10:17:36 +0800

    项目初始化

---
+++ src/com/aijf/structures/array/SinglyLinkedList.java
@@ -0,0 +1,13 @@
+package com.aijf.structures.array;
+
+/**
+ * @author : zhaochuanzhen
+ * @description : 单链表
+ * @date : 2019/12/16 10:15
+ */
+public class SinglyLinkedList {
+
+    public static void main(String[] args){
+        System.out.println(3252);
+    }
+}
